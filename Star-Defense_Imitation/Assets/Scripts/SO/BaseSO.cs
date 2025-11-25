3ebde307a7dbf81c282fcdb626fe4bcf1a5890e4
kimjun3333 <kheami35@gmail.com>
2025-11-25 14:08:47 +0900

    [Feat] BaseSO

---
+++ Star-Defense_Imitation/Assets/Scripts/SO/BaseSO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// SO의 베이스가 되는 스크립트
+/// </summary>
+public class BaseSO : ScriptableObject
+{
+    public string ID;
+    public string Name;
+
+    public string SpriteID;
+    public Sprite Sprite; 
+}
