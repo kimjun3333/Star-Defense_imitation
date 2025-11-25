6cc1aee6f5f63dec7849c80ca5eab369e10b5a00
kimjun3333 <kheami35@gmail.com>
2025-11-25 14:34:39 +0900

    [Fix] TowerSO 변수 Enum으로 변경

--- Star-Defense_Imitation/Assets/Scripts/SO/TowerSO.cs
+++ Star-Defense_Imitation/Assets/Scripts/SO/TowerSO.cs
@@ -8,12 +8,10 @@ public class TowerSO : BaseSO
     public float Range; //사거리
     public float AtkSpeed; //공격속도 
 
-    //근거리인지 원거리인지가 필요하네
-
-    //TODO 아래내용들은 Enum으로 교체할 예정
-    public string Rarity; //등급 Enum으로 변경 예정
-    public string TargetType; //대상타입 Enum으로 변경 예정
-    public string Tribe; //종족 => 패시브 스킬개념
+    public AttackType AttackType; //근거리 or 원거리
+    public RarityType Rarity; //등급
+    public UnitType TargetType; //대공 , 지공 , 지대공
+    public TribeType Tribe; //종족 => 패시브 스킬개념
 
     public string SkillID; //스킬 보유 => SO생성시 ID로 매핑할 예정
 }
