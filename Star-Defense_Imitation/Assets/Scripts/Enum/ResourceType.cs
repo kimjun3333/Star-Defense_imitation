eb1cf77b3783e21761ab7c011b959cecbb6e0c7c
kimjun3333 <kheami35@gmail.com>
2025-11-25 17:28:40 +0900

    [Feat] EnemySO

---
+++ Star-Defense_Imitation/Assets/Scripts/Enum/ResourceType.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//자원 타입
+public enum ResourceType
+{
+    Gold, //골드
+    Mineral //미네랄
+}
---
+++ Star-Defense_Imitation/Assets/Scripts/SO/EnemySO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySO : BaseSO
+{
+    public float Health;
+    public float Speed;
+
+    public UnitType UnitType;
+    public ResourceType RewardType; //골드 Or 미네랄
+    public int Reward;
+}
