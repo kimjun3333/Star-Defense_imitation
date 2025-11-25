827dd91608ba71e3992bd1c246374a07f2405abf
kimjun3333 <kheami35@gmail.com>
2025-11-25 14:22:05 +0900

    [Feat] TowerSO

---
+++ Star-Defense_Imitation/Assets/Scripts/SO/TowerSO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSO : BaseSO
+{
+    public float Dmg; //공격력
+    public float Range; //사거리
+    public float AtkSpeed; //공격속도 
+
+    //TODO 아래내용들은 Enum으로 교체할 예정
+    public string Rarity; //등급 Enum으로 변경 예정
+    public string TargetType; //대상타입 Enum으로 변경 예정
+    public string Tribe; //종족 => 패시브 스킬개념
+    public string SkillID; //스킬 보유 => SO생성시 ID로 매핑할 예정
+}
