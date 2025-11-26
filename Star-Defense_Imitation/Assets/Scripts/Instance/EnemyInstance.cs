aa6352b9a7662bdd1082b749f658ebcf24ce66b5
kimjun3333 <kheami35@gmail.com>
2025-11-26 09:19:57 +0900

    [Feat] EnemyInstance
    SO 기반으로 Instance

---
+++ Star-Defense_Imitation/Assets/Scripts/Instance/EnemyInstance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+[Serializable]
+public class EnemyInstance
+{
+    public EnemySO Definition; //원본SO
+    public float CurrentHealth; //현재 체력
+    public float CurrentSpeed; //현재 속도
+
+    public int CurrentWaypointIndex; //경로 이동용
+
+    public EnemyInstance(EnemySO so)
+    {
+        Definition = so;
+
+        CurrentHealth = so.Health;
+        CurrentSpeed = so.Speed;
+        CurrentWaypointIndex = 0;
+    }
+}
