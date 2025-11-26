f9854733b26a91f9ac3a2248d12b4824248283fc
kimjun3333 <kheami35@gmail.com>
2025-11-26 10:14:07 +0900

    [Test] EnemySpawner 테스트용

---
+++ Star-Defense_Imitation/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public EnemyController enemyPrefab;
+    public Path path; //경로
+    public EnemySO testEnemy;
+
+    public void SpawnEnemy(EnemySO enemySO)
+    {
+        EnemyController enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+        enemy.Init(enemySO, path.Waypoints);
+    }
+
+    public void TestSpawn()
+    {
+        SpawnEnemy(testEnemy);
+    }
+
+    private void Start()
+    {
+        Invoke(nameof(TestSpawn), 2f);
+    }
+}
---
+++ Star-Defense_Imitation/Assets/Scripts/Path.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    public Transform[] Waypoints;
+
+    private void Start()
+    {
+        Waypoints = new Transform[transform.childCount];
+
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            Waypoints[i] = transform.GetChild(i);
+        }
+    }
+}
