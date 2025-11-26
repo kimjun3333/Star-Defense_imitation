0ec2c55abaf99a28ecd79839c4e58fe68ecb5e76
kimjun3333 <kheami35@gmail.com>
2025-11-26 09:28:56 +0900

    [Feat] EnemyController

---
+++ Star-Defense_Imitation/Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class EnemyController : MonoBehaviour
+{
+    private EnemyInstance instance;
+
+    private Transform[] waypoints;
+    private int waypointIndex = 0;
+
+    public void Init(EnemySO so, Transform[] path)
+    {
+        instance = new EnemyInstance(so);
+        waypoints = path;
+        waypointIndex = 0;
+        transform.position = waypoints[0].position;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex >= waypoints.Length) return;
+
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            waypoints[waypointIndex].position,
+            instance.CurrentSpeed * Time.deltaTime
+            );
+
+        if(Vector3.Distance(transform.position, waypoints[waypointIndex].position) < 0.1f)
+        {
+            waypointIndex++;
+            if(waypointIndex >= waypoints.Length)
+            {
+                Debug.Log("골인");
+            }
+        }
+    }
+
+}
--- Star-Defense_Imitation/Assets/Scripts/Enemy/EnemyInstance.cs
+++ Star-Defense_Imitation/Assets/Scripts/Enemy/EnemyInstance.cs
@@ -7,14 +7,11 @@ public class EnemyInstance
     public float CurrentHealth; //현재 체력
     public float CurrentSpeed; //현재 속도
 
-    public int CurrentWaypointIndex; //경로 이동용
-
     public EnemyInstance(EnemySO so)
     {
         Definition = so;
 
         CurrentHealth = so.Health;
         CurrentSpeed = so.Speed;
-        CurrentWaypointIndex = 0;
     }
 }
