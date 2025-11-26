8400f8ef0f4b74bdad1e1005604da25b842717b9
kimjun3333 <kheami35@gmail.com>
2025-11-26 12:26:56 +0900

    [Feat] WaveSO, StageSO

---
+++ Star-Defense_Imitation/Assets/Scripts/SO/StageSO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSO : BaseSO
+{
+    public string MapPrefabID; //스테이지 맵
+    public string[] WaveIDs;
+}
---
+++ Star-Defense_Imitation/Assets/Scripts/SO/WaveSO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WaveSO",menuName = "SO/WaveSO")]
+
+public class WaveSO : BaseSO
+{
+    public List<SpawnInfo> Enemies;
+}
+
+[Serializable]
+public class SpawnInfo
+{
+    public string EnemyID;
+    public int Count;
+}
