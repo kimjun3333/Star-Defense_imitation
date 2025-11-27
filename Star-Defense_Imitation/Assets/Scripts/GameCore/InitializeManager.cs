1a742b7bb9173d97d7b5716fdbc19ac42cc81f84
kimjun3333 <kheami35@gmail.com>
2025-11-27 10:28:36 +0900

    [Feat] 초기화 매니저

---
+++ Star-Defense_Imitation/Assets/Scripts/GameCore/InitializeManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 초기화 매니저
+/// </summary>
+public class InitializeManager : Singleton<InitializeManager>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+
+        Debug.Log("초기화 시작");
+    }
+}
