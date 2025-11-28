6320d2652c172cd4f194e371040a5ebab601c88c
kimjun3333 <kheami35@gmail.com>
2025-11-28 14:55:02 +0900

    [Feat] TowerManager

---
+++ Star-Defense_Imitation/Assets/Scripts/GameCore/TowerManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+/// <summary>
+/// 타워 관리를 하는 매니저 클래스
+/// </summary>
+public class TowerManager : Singleton<TowerManager>, IInitializable
+{
+    private GameObject towerPrefab; // 시작할때 등록하고
+
+    [SerializeField] private List<TowerController> towers = new(); //설치한 타워목록
+    public Task Init()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public TowerController BuildTower(TowerSO so, Vector3 pos)
+    {
+        GameObject obj = Instantiate(towerPrefab, pos, Quaternion.identity);
+
+        TowerController tower = obj.GetComponent<TowerController>();
+        tower.Init(so);
+
+        towers.Add(tower);
+        return tower;
+    }
+
+    public void RemoveTower(TowerController tower)
+    {
+        towers.Remove(tower);
+        Destroy(tower.gameObject);
+    }
+}
