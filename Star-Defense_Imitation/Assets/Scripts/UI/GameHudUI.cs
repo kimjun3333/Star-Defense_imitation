9ba0e4b5354152824866f08992337c4393f55831
kimjun3333 <kheami35@gmail.com>
2025-11-29 13:57:06 +0900

    [Feat] GameHudUI

---
+++ Star-Defense_Imitation/Assets/Scripts/UI/GameHudUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameHudUI : UIBase
+{
+    [Header("Resources Text")]
+    [SerializeField] private TextMeshProUGUI goldText;
+    [SerializeField] private TextMeshProUGUI mineralText;
+
+    [Header("Wave Text")]
+    [SerializeField] private TextMeshProUGUI waveText;
+
+    public override void OnInit()
+    {
+        base.OnInit();
+    }
+
+    public override void OnOpen()
+    {
+        base.OnOpen();
+    }
+
+    public override void OnClose()
+    {
+        base.OnClose();
+    }
+}
