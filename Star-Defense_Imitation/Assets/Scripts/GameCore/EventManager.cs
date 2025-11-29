501ad6c58694849e92cc7a0f5e3516e0cac475ba
kimjun3333 <kheami35@gmail.com>
2025-11-29 14:09:05 +0900

    [Feat] EventMangaer 초기버전

---
+++ Star-Defense_Imitation/Assets/Scripts/GameCore/EventManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EventType
+{
+    ResourceChanged,
+    WaveChanged,
+    EnemyDied,
+}
+public class EventManager : Singleton<EventManager>
+{
+    private readonly Dictionary<EventType, Action<object>> eventTable = new();
+
+    /// <summary>
+    /// 이벤트 구독
+    /// </summary>
+    public void Subscribe(EventType type, Action<object> callback)
+    {
+        if (!eventTable.ContainsKey(type))
+            eventTable[type] = null;
+
+        eventTable[type] += callback;
+    }
+
+    /// <summary>
+    /// 이벤트 해제
+    /// </summary>
+    public void Unsubscribe(EventType type, Action<object> callback)
+    {
+        if (eventTable.ContainsKey(type))
+            eventTable[type] -= callback;
+    }
+
+    /// <summary>
+    /// 이벤트 호출
+    /// </summary>
+    public void Trigger(EventType type, object payload = null)
+    {
+        if(eventTable.TryGetValue(type, out var action) && action != null)
+        {
+            action.Invoke(payload);
+        }
+    }
+}
